625c1706a1dad8012c1e2f14e4c0c1273feba9d2
user <shreyaksambitpatnaik@gmail.com>
2020-03-31 16:26:35 +0530

    The First Commit of Sikuli

---
+++ SikuliConcept/src/main/java/SikuliConcept/SikuliConcept/YouTubeImageTest.java
@@ -0,0 +1,29 @@
+package SikuliConcept.SikuliConcept;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class YouTubeImageTest
+{
+
+	public static void main(String[] args)
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Desktop\\Selenium_Naveen\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		
+		driver.get("https://www.youtube.com/watch?v=OtQAMoXYqes&list=PLFGoYjJG_fqo4oVsa6l_V-_7-tzBnlulT&index=22");
+		
+		Screen screen = new Screen();
+
+	}
+
+}
