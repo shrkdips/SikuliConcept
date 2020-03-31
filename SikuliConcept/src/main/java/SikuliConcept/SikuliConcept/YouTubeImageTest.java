6262fef3a04b75b2378ca9af69e7d0e8cb10868d
user <shreyaksambitpatnaik@gmail.com>
2020-03-31 16:29:41 +0530

    Updated Screen Class

--- SikuliConcept/src/main/java/SikuliConcept/SikuliConcept/YouTubeImageTest.java
+++ SikuliConcept/src/main/java/SikuliConcept/SikuliConcept/YouTubeImageTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.sikuli.api.Screen;
 
 public class YouTubeImageTest
 {
